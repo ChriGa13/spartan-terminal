016bbb3d5792e5190e80c42c62a0576e121decbc
Christoph Gattermayr <christoph.gattermayr@outlook.com>
2021-05-02 21:53:33 +0200

    Improve installer through copying configs to their locations

--- spartan/spartan_installer.ps1
+++ spartan/spartan_installer.ps1
@@ -1,6 +1,20 @@
+# ::ENTRY POINT
+
+# Check if script is started with admin rights (needed for installing modules)
+If(-NOT([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Warning "Please re-run this script as an Administrator!"
+    Break
+}
+
+# Set config paths
+$SettingsPath = "C:\Users\chris\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+$ProfilePath = "C:\Users\chris\OneDrive\Private\Documents\WindowsPowerShell"
+
 # Installing necessary modules
+Install-Module posh-git -Scope AllUsers
 Install-Module oh-my-posh -Scope AllUsers
-Install-Module -Name Terminal-Icons
+Install-Module -Name Terminal-Icons -Scope AllUsers
 
 # Installing fonts
 $Source = ".\resources\fonts\Meslo.zip"
@@ -10,29 +24,34 @@ Expand-Archive $Source -DestinationPath $FontsFolder -Force
 
 $FONTS = 0x14
 $CopyOptions = 4 + 16;
-$objShell = New-Object -ComObject Shell.Application
-$objFolder = $objShell.Namespace($FONTS)
-$allFonts = dir $FontsFolder
+$ObjShell = New-Object -ComObject Shell.Application
+$ObjFolder = $ObjShell.Namespace($FONTS)
+$AllFonts = Get-ChildItem $FontsFolder
 
 foreach($font in Get-ChildItem -Path $fontsFolder -File)
 {
     $dest = "C:\Windows\Fonts\$font"
     If(Test-Path -Path $dest)
     {
-        echo "Font $font already installed"
+        Write-Output "Font $font already installed"
     }
     Else
     {
-        echo "Installing $font"
+        Write-Output "Installing $font"
         $CopyFlag = [String]::Format("{0:x}", $CopyOptions);
-        $objFolder.CopyHere($font.fullname,$CopyFlag)
+        $ObjFolder.CopyHere($font.fullname,$CopyFlag)
     }
 }
 
 # Copy / replace settings.json in windows terminal installation folder
-# TODO: copy / replace
+Copy-Item .\resources\settings\settings.json -Destination $SettingsPath -force
 
 # Create new powershell profile and override it with existing profile
 Test-Path $PROFILE
 New-Item -Type File -Force $PROFILE
-# TODO: override
+Copy-Item .\resources\settings\Microsoft.PowerShell_profile.ps1 -Destination $ProfilePath -force
+
+# TODO: generate new profile guid and replace it with the one in copied settings.json (under 'defaultProfile')
+# $ProfileGuid = [guid]::NewGuid()
+
+# ::END
